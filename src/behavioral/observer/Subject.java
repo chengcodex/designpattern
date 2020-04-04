b839d0699231b51f962e9c315a978cf6f3801520
xiao <1204598429@qq.com>
2020-04-04 10:59:12 +0800

    观察者模式

---
+++ src/behavioral/observer/Subject.java
@@ -0,0 +1,10 @@
+package behavioral.observer;
+
+
+public interface Subject {
+    void registerObserver(Observer o);
+
+    void removeObserver(Observer o);
+
+    void notifyObserver();
+}
---
+++ src/behavioral/observer/WeatherStationClient.java
@@ -0,0 +1,12 @@
+package behavioral.observer;
+
+public class WeatherStationClient {
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+        CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+
+        weatherData.setMeasurements(0, 0, 0);
+        weatherData.setMeasurements(1, 1, 1);
+    }
+}
